f97d8f88d0c22a03651793cb6b23918a4d5640ab
eky2429 <eky2429@gmail.com>
2024-12-12 14:33:49 -0600

    Updated code for Savings application!

--- main.py
+++ main.py
@@ -1,26 +1,67 @@
-from decimal import Decimal
+# Savings project script
+needs : float = 0.5
+wants : float = 0.3
+savings : float = 0.2
 
-#This is the main file for my budget program
-amount = float(input("How much did you make? "))
-needs = amount * 0.5
-wants = amount * 0.3
-savings = amount * 0.2
+results : list = []
 
-print("Needs: " + str(needs))
-print("Wants: " + str(wants))
-print("Savings: " + str(savings))
+def process_input(s: float):
+    results.append(needs * s)
+    results.append(wants * s)
+    results.append(savings * s)
 
-elements_to_round = {"needs": 0, "wants": 0, "savings": 0}
+    print("\nResults:")
 
-if needs % 0.01 != 0:
-    elements_to_round["needs"] = round(needs % 0.01, 3)
+    total: float = 0.0
+    for i in range(0, len(results)):
+        results[i] = round(results[i], 2)
+        total += results[i]
+    total = round(total, 2)
 
-if wants % 0.01 != 0:
-    elements_to_round["wants"] = round(wants % 0.01, 3)
+    print("Needs: " + str(results[0]))
+    print("Wants: " + str(results[1]))
+    print("Savings: " + str(results[2]))
+    print("Is accurate: " + str(total == s))
 
-if savings % 0.01 != 0:
-    elements_to_round["savings"] = round(savings % 0.01, 3)
+def debug_process_input(s: float):
+    results.append(needs * s)
+    results.append(wants * s)
+    results.append(savings * s)
 
-print("Stuff to round: " + str(elements_to_round) )
+    print("\nResults:")
+    print("Needs: " + str(results[0]))
+    print("Wants: " + str(results[1]))
+    print("Savings: " + str(results[2]))
 
-print("\nHave a good day! :)")
+    total: float = 0.0
+    for i in range(0, len(results)):
+        total += results[i]
+        results[i] = round(results[i], 2)
+    print("Is accurate: " + str(total == s))
+
+    print("\nRounded results:")
+    total = 0.0
+    for i in range(0, len(results)):
+        total += results[i]
+    total = round(total, 2)
+
+    print("Needs: " + str(results[0]))
+    print("Wants: " + str(results[1]))
+    print("Savings: " + str(results[2]))
+    print("Is accurate: " + str(total == s))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Hello, user!")
+
+    print("This is the savings program")
+    print("This will allow you to calculate how much to put into needs, wants, and savings.")
+    print("So, here's how it works: type in a number, and then results will be outputted.")
+    print("Input: ")
+
+    salary : float =  float(input())
+    process_input(salary)
+
+    print("\nGood-bye")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
