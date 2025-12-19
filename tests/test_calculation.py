4b37f451e8aead97d7bc1597ece493fb771b0d18
Ethan Ky <eky2429@gmail.com>
2025-12-18 19:36:00 -0600

    Created test cases (YET TO TEST!)

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -5,8 +5,106 @@ def test_basic_rule():
     assert result["Needs"] == 500
     assert result["Wants"] == 500
 
-def test_balanced_rounding():
-    rule = {"Needs": 51, "Wants": 49}
-    out = calculate_allocations(50, rule)
+def test_basic_rule2():
+    salary = 1000
 
-    assert round(sum(out.values()), 2) == 50
+    from core.rules import prebuilt_rules
+    result = calculate_allocations(salary, prebuilt_rules["50/30/20"])
+    assert result["Needs"] == 500
+    assert result["Wants"] == 300
+    assert result["Savings"] == 200
+
+    total = 0
+    for num in result.values():
+        total += num
+    assert total == salary
+
+def test_balanced_rounding():
+    salary = 50
+
+    rule = {"Needs": 51, "Wants": 49}
+    out = calculate_allocations(salary, rule)
+
+    assert out["Needs"] == 25.5
+    assert out["Wants"] == 24.5
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
+
+def test_round_mismatch():
+    salary = 100
+
+    rule = {"A": 33, "B": 33, "C": 34}
+    out = calculate_allocations(salary, rule)
+
+    assert out["A"] == 33
+    assert out["B"] == 33
+    assert out["C"] == 34
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
+
+def test_fractional_rule():
+    salary = 100
+
+    rule = {"A": 33, "B": 33, "C": 34}
+    out = calculate_allocations(salary, rule)
+
+    assert out["A"] == 33
+    assert out["B"] == 33
+    assert out["C"] == 34
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
+
+def test_complex_fractional_rule():
+    salary = 999.99
+
+    rule = {"A": 25.5, "B": 25.5, "C": 25.5, "D": 23.5}
+    out = calculate_allocations(salary, rule)
+
+    for key in rule.keys():
+        assert out[key] == rule[key] * salary
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
+
+def test_many_bins():
+    salary = 500
+    rule = {"A": 5, "B": 5, "C": 5, "D": 5, "E": 5,
+      "A1": 5, "B1": 5, "C1": 5, "D1": 5, "E1": 5,
+      "A2": 5, "B2": 5, "C2": 5, "D2": 5, "E2": 5,
+      "A3": 5, "B3": 5, "C3": 5, "D3": 5, "E3": 5}
+
+    out = calculate_allocations(salary, rule)
+
+    for key in rule.keys():
+        assert out[key] == rule[key] * salary
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
+
+def test_small_salary():
+    salary = 0.03
+    rule = {"A": 50, "B": 50}
+
+    out = calculate_allocations(salary, rule)
+
+    expected_values = {"A": 0.02, "B": 0.01}
+    for key in rule.keys():
+        assert out[key] == expected_values[key]
+
+    total = 0
+    for num in out.values():
+        total += num
+    assert total == salary
---
+++ tests/test_rules.py
@@ -0,0 +1,30 @@
+from core.rules import is_valid_rule, prebuilt_rules
+
+def test_prebuilt_rules():
+    for value in prebuilt_rules.values():
+        assert(is_valid_rule(value) == True)
+
+def test_custom_valid_rule():
+    rule = {"A": 40, "B": 30, "C": 30}
+    assert(is_valid_rule(rule) == True)
+
+def test_custom_invalid_rule():
+    rule = {"A": 40, "B": 40, "C": 10}
+    assert(is_valid_rule(rule) != True)
+
+def test_few_bins():
+    rules = [{}, {"A": 100}]
+    for rule in rules:
+        assert(is_valid_rule(rule) == False)
+
+def test_many_bins():
+    rule = {"A": 5, "B": 5, "C": 5, "D": 5, "E": 5,
+      "A1": 5, "B1": 5, "C1": 5, "D1": 5, "E1": 5,
+      "A2": 5, "B2": 5, "C2": 5, "D2": 5, "E2": 5,
+      "A3": 5, "B3": 5, "C3": 5, "D3": 5, "E3": 2,
+            "A4": 3,}
+    assert(is_valid_rule(rule) == False)
+
+def test_small_percentage():
+    rule = {"A": 1, "B": 99}
+    assert(is_valid_rule(rule) == False)
